d5aa96f8467d4f49d8bb44ce3ee8324e15ee7a68
am1DAM2 <xinboliu555@gmail.com>
2025-12-02 12:45:47 +0100

    arreglando problemas

--- app/src/main/java/com/example/nicestart/MainActivity.java
+++ app/src/main/java/com/example/nicestart/MainActivity.java
@@ -1,27 +1,62 @@
 package com.example.nicestart;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.bumptech.glide.Glide;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView mycontext = findViewById(R.id.myText);
+        registerForContextMenu(mycontext);
+
+        swipeLayout = findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+    }
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = ()->{
+        Toast toast0 = Toast.makeText(MainActivity.this, "Hithere!",Toast.LENGTH_LONG);
+        toast0.show();
+        swipeLayout.setRefreshing(false);
+    };
+
+    // IMPLEMENTING CONTEXT MENU
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        getMenuInflater().inflate(R.menu.menu_context, menu);
     }
 
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
+            toast.show();
+        } else if(item.getItemId() == R.id.item2) {
+            Toast toast2 = Toast.makeText(this,"Downloading item...", Toast.LENGTH_LONG);
+            toast2.show();
+        }
+        return false;
+    }
 
+    //IMPLEMENTING APPBAR MENU
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_appbar,menu);
+        return true;
+    }
 }
